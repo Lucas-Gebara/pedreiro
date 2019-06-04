953fc14d14d5a816950b9e09b4d95e343bc64632
Vincent Koester <vincent.koester@gmx.de>
2019-06-04 18:26:18 -0300

    Seeds created

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,48 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+if Rails.env.development?
+  puts "deleting database"
+  Offer.destroy_all
+  User.destroy_all
+end
+
+puts "creating users"
+user1 = User.create!(
+  name: "vince",
+  address: "gloria",
+  cpf: "123456",
+  phone: "19838498",
+  password: "hallooooo",
+  email: "vince13@gmail.com"
+)
+user2 = User.create!(
+  name: "lukas",
+  address: "copacabana",
+  cpf: "654321",
+  phone: "9585749",
+  password: "holaaaaa",
+  email: "lukas007@gmail.com"
+)
+user3 = User.create!(name: "filipe",
+  address: "tijuca",
+  cpf: "5969869",
+  phone: "898385",
+  password: "namasteeeee",
+  email: "filipe1@gmail.com"
+)
+
+puts "creating offers"
+Offer.create!(
+  title: "Gardening",
+  description: "I care about your garden",
+  user: user1
+)
+Offer.create!(
+  title: "fix washingmachine",
+  description: "i will fix your washing machine",
+  user: user2
+)
+Offer.create!(
+  title: "washing",
+  description: "i will wash your clothes",
+  user: user3
+)
+

57a64e3268752d45ae553a186c702aa2212e0dbf
Lucas Gebara <lucasgebara@outlook.com>
2019-06-04 14:41:00 -0300

    offer routes updated

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,9 @@ Rails.application.routes.draw do
   root to: "offers#index"
 
   devise_for :users
+  resources :portfolio, only: [:destroy]# For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :offers, only: [:index, :new, :create, :destroy]
+
   resources :users do
     resources :portfolio, except: [:index, :destroy, :edit, :update]
   end
@@ -9,5 +12,4 @@ Rails.application.routes.draw do
   resources :bookings ,except: [:destroy] do
     resources :reviews, only: [:new, :create, :index]
   end
-  resources :portfolio, only: [:destroy]# For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end

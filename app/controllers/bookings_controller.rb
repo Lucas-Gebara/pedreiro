31cba8abdc293c7b49e1202b7bb488d5b8e61b28
Filipe Costa <lfilipe82@yahoo.com.br>
2019-06-04 17:05:54 -0300

    booking controller changed

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,2 +1,22 @@
 class BookingsController < ApplicationController
+  def new
+    @user = current_user
+    @offer = Offer.find(params[:offer_id])
+    @booking = Booking.new
+  end
+
+  def create
+    @booking = Booking.new(booking_params)
+    if @booking.save
+      redirect_to user_path
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def booking_params
+    params.require(:booking).permit(:user_id, :offer_id)
+  end
 end

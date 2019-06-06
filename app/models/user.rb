a7aa204e504ee66c20fca2a7bcae2507de33eb7c
Vincent Koester <vincent.koester@gmx.de>
2019-06-06 12:29:04 -0300

    Added Avatar Function to the navbar

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,8 +3,8 @@ class User < ApplicationRecord
 
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
   has_many :offers, dependent: :destroy
   has_one  :portfolio, dependent: :destroy
   has_many :bookings, dependent: :destroy
